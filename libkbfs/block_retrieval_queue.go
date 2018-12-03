db12e3cba74dcff85ff22e2df6780457b476d26d
Jeremy Stribling <strib@alum.mit.edu>
2018-12-03 14:09:14 -0800

    prefetcher: track bytes fetched and bytes total for each prefetch
    Issue: KBFS-3575

--- libkbfs/block_retrieval_queue.go
+++ libkbfs/block_retrieval_queue.go
@@ -35,6 +35,7 @@ type blockRetrievalPartialConfig interface {
 	diskBlockCacheGetter
 	syncedTlfGetterSetter
 	initModeGetter
+	clockGetter
 }
 
 type blockRetrievalConfig interface {
--- libkbfs/block_retrieval_worker_test.go
+++ libkbfs/block_retrieval_worker_test.go
@@ -101,17 +101,32 @@ func (bg *fakeBlockGetter) assembleBlock(ctx context.Context,
 	return nil
 }
 
+const testFakeBlockSize = uint32(150)
+
 func makeFakeFileBlock(t *testing.T, doHash bool) *FileBlock {
 	buf := make([]byte, 16)
 	_, err := rand.Read(buf)
 	require.NoError(t, err)
 	block := &FileBlock{
+		CommonBlock: CommonBlock{
+			cachedEncodedSize: testFakeBlockSize,
+		},
 		Contents: buf,
 	}
 	if doHash {
 		_ = block.GetHash()
 	}
 	return block
+}
+
+func makeFakeFileBlockWithIPtrs(iptrs []IndirectFilePtr) *FileBlock {
+	return &FileBlock{
+		CommonBlock: CommonBlock{
+			IsInd:             true,
+			cachedEncodedSize: testFakeBlockSize,
+		},
+		IPtrs: iptrs,
+	}
 }
 
 func TestBlockRetrievalWorkerBasic(t *testing.T) {
@@ -326,7 +341,8 @@ func TestBlockRetrievalWorkerMultipleBlockTypes(t *testing.T) {
 	_, continueCh1 := bg.setBlockToReturn(ptr1, block1)
 	testCommonBlock := &CommonBlock{}
 	testCommonBlock.Set(block1)
-	require.Equal(t, &CommonBlock{}, testCommonBlock)
+	require.Equal(
+		t, &CommonBlock{cachedEncodedSize: testFakeBlockSize}, testCommonBlock)
 
 	t.Log("Make a retrieval for the same block twice, but with a different " +
 		"target block type.")
